ce3bb1c16ece09507268c4ed81c74750764634c0
Md Shykat <61962850+pain-arch@users.noreply.github.com>
2021-09-14 21:24:07 +0600

    Update app.js

--- temperature-hot-solution-main/js/app.js
+++ temperature-hot-solution-main/js/app.js
@@ -1,4 +1,4 @@
-const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
+const API_KEY = ``;
 const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
